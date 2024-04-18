60f7f71e79bd0a176689daa638fff04103921413
surrealtm <unreal.hiraeth@gmail.com>
2024-04-18 13:14:49 +0200

    Added a basic thread module.

---
+++ src/threads.cpp
@@ -0,0 +1,203 @@
+#include "threads.h"
+
+#if FOUNDATION_WIN32
+# define WIN32_LEAN_AND_MEAN
+# include <windows.h>
+
+struct Thread_Win32_State {
+    b8 setup;
+    HANDLE handle;
+    CRITICAL_SECTION mutex;
+    CONDITION_VARIABLE signal;
+};
+
+static_assert(sizeof(Thread_Win32_State) <= sizeof(Thread::platform_data), "Thread_Win32_State is bigger than expected.");
+
+void __thread_internal_cleanup(Thread_Win32_State *win32) {
+    if(!win32->setup) return;
+    DeleteCriticalSection(&win32->mutex);
+    win32->handle = INVALID_HANDLE_VALUE;
+    win32->setup = false;
+}
+
+
+struct Mutex_Win32_State {
+    CRITICAL_SECTION handle;
+};
+
+static_assert(sizeof(Mutex_Win32_State) <= sizeof(Mutex::platform_data), "Mutex_Win32_State is bigger than expected.");
+
+#endif
+
+
+
+/* ------------------------------------------------ Thread API ------------------------------------------------ */
+
+Thread create_thread(Thread_Entry_Point procedure, void *user_pointer, b8 start_as_suspended) {
+    Thread thread;
+
+#if FOUNDATION_WIN32
+    Thread_Win32_State *win32 = (Thread_Win32_State *) thread.platform_data;
+    win32->setup = false;
+    win32->handle = CreateThread(null, 0, procedure, user_pointer, 0, null);
+
+    if(win32->handle) {
+        InitializeCriticalSection(&win32->mutex);
+        InitializeConditionVariable(&win32->signal);
+        win32->setup = true;
+        thread.state = THREAD_STATE_Running;
+    } else {
+        thread.state = THREAD_STATE_Running; // We should probably give a better error here...
+    }
+#endif
+
+    if(start_as_suspended) suspend_thread(&thread); // This is a bit ugly, but the pthread library does not have built-in support for this.
+    
+    return thread;
+}
+
+void join_thread(Thread *thread) {
+    if(thread->state == THREAD_STATE_Terminated) return;
+
+    if(thread->state == THREAD_STATE_Suspending || thread->state == THREAD_STATE_Suspended) {
+        //
+        // The thread must exit gracefully on join, so resume it, and make sure that it
+        // actually received the resume signal. If not, we would endlessly wait while it
+        // was suspended.
+        //
+        resume_thread(thread);
+    }
+
+#if FOUNDATION_WIN32
+    Thread_Win32_State *win32 = (Thread_Win32_State *) thread->platform_data;
+    if(!win32->setup) return;
+
+    b8 this_call_destroyed_thread = false;
+    
+    EnterCriticalSection(&win32->mutex);
+
+    // Make sure the thread has received its resuming signal and is now running again. This prevents
+    // the issue where we snatch the signal before the thread_wait_if_suspended procedure, in which case
+    // the thread would never awaken, and never properly exit.
+    while(thread->state == THREAD_STATE_Resuming) {
+        SleepConditionVariableCS(&win32->signal, &win32->mutex, INFINITE);
+    }
+
+    if(thread->state != THREAD_STATE_Terminated) {
+        // Only attempt to join if the thread is running, and has not since terminated.
+        thread->state = THREAD_STATE_Terminating;
+        WaitForSingleObject(win32->handle, INFINITE);
+        CloseHandle(win32->handle);
+        thread->state = THREAD_STATE_Terminated;
+        this_call_destroyed_thread = true;
+    }
+    
+    LeaveCriticalSection(&win32->mutex);
+    if(this_call_destroyed_thread) __thread_internal_cleanup(win32);
+#endif
+}
+
+void detach_thread(Thread *thread) {
+    if(thread->state == THREAD_STATE_Terminated || thread->state == THREAD_STATE_Terminating) return;
+
+#if FOUNDATION_WIN32
+    Thread_Win32_State *win32 = (Thread_Win32_State *) thread->platform_data;
+    if(!win32->setup) return;
+
+    b8 this_call_destroyed_thread = true;
+    
+    EnterCriticalSection(&win32->mutex);
+
+    if(thread->state != THREAD_STATE_Terminated && thread->state != THREAD_STATE_Detached) {
+        CloseHandle(win32->handle);
+        thread->state = THREAD_STATE_Detached;
+    }
+
+    LeaveCriticalSection(&win32->mutex);
+    if(this_call_destroyed_thread) __thread_internal_cleanup(win32);
+#endif
+}
+
+void suspend_thread(Thread *thread) {
+    if(thread->state != THREAD_STATE_Running) return;
+
+#if FOUNDATION_WIN32
+    Thread_Win32_State *win32 = (Thread_Win32_State *) thread->platform_data;
+    if(!win32->setup) return;
+
+    EnterCriticalSection(&win32->mutex);
+    thread->state = THREAD_STATE_Suspending;
+    LeaveCriticalSection(&win32->mutex);
+#endif
+}
+
+void resume_thread(Thread *thread) {
+    if(thread->state != THREAD_STATE_Suspended) return;
+
+#if FOUNDATION_WIN32
+    Thread_Win32_State *win32 = (Thread_Win32_State *) thread->platform_data;
+    if(!win32->setup) return;
+
+    EnterCriticalSection(&win32->mutex);
+    thread->state = THREAD_STATE_Resuming;
+    WakeAllConditionVariable(&win32->signal);
+    LeaveCriticalSection(&win32->mutex);
+#endif
+}
+
+void thread_wait_if_suspended(Thread *thread) {
+    if(thread->state != THREAD_STATE_Suspended) return;
+
+#if FOUNDATION_WIN32
+    Thread_Win32_State *win32 = (Thread_Win32_State *) thread->platform_data;
+    if(!win32->setup) return;
+
+    EnterCriticalSection(&win32->mutex);
+    while(thread->state == THREAD_STATE_Suspended) {
+        SleepConditionVariableCS(&win32->signal, &win32->mutex, INFINITE);
+    }
+
+    thread->state = THREAD_STATE_Running;
+    WakeAllConditionVariable(&win32->signal); // Signal back that we have resumed (e.g. to the join_thread procedure).
+    
+    LeaveCriticalSection(&win32->mutex);
+#endif
+}
+
+void thread_sleep(f32 seconds) {
+#if FOUNDATION_WIN32
+    Sleep((DWORD) (seconds * 1000.0f));
+#endif
+}
+
+
+
+/* ------------------------------------------------ Mutex API ------------------------------------------------ */
+
+void create_mutex(Mutex *mutex) {
+#if FOUNDATION_WIN32
+    Mutex_Win32_State *win32 = (Mutex_Win32_State *) mutex->platform_data;
+    InitializeCriticalSection(&win32->handle);
+#endif
+}
+
+void destroy_mutex(Mutex *mutex) {
+#if FOUNDATION_WIN32
+    Mutex_Win32_State *win32 = (Mutex_Win32_State *) mutex->platform_data;
+    DeleteCriticalSection(&win32->handle);
+#endif
+}
+
+void lock(Mutex *mutex) {
+#if FOUNDATION_WIN32
+    Mutex_Win32_State *win32 = (Mutex_Win32_State *) mutex->platform_data;
+    EnterCriticalSection(&win32->handle);
+#endif
+}
+
+void unlock(Mutex *mutex) {
+#if FOUNDATION_WIN32
+    Mutex_Win32_State *win32 = (Mutex_Win32_State *) mutex->platform_data;
+    LeaveCriticalSection(&win32->handle);
+#endif
+}
---
+++ src/threads.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "foundation.h"
+
+
+/* The threading API is supposed to make multithreading platform independent and a bit easier. It gives access to
+ * threads and mutexes.
+ * Threads can be created and then either joined or detached. This module also supports suspending threads on
+ * a user level (which can be really useful for e.g. a job system). This means that any thread can mark another
+ * as suspended, but this thread can decide when (and if) to wait until it is resumed in user code (see
+ * thread_wait_if_suspended). Until then, the thread is still considered to be running.
+ * This module also supports basic mutexes.
+ */
+
+
+
+/* ------------------------------------------------ Thread API ------------------------------------------------ */
+
+typedef u32(*Thread_Entry_Point)(void*);
+
+#define THREAD_INTERNAL_STATE_SIZE 64 // This is the highest size of internal platform data needed to be stored, to avoid a memory allocation here...
+
+enum Thread_State {
+    THREAD_STATE_Running     = 0x0,
+    THREAD_STATE_Suspending  = 0x1,
+    THREAD_STATE_Suspended   = 0x2,
+    THREAD_STATE_Resuming    = 0x3,
+    THREAD_STATE_Terminating = 0x4,
+    THREAD_STATE_Terminated  = 0x5,
+    THREAD_STATE_Detached    = 0x6,
+};
+
+struct Thread {
+    u8 platform_data[THREAD_INTERNAL_STATE_SIZE];
+    Thread_State state;        
+};
+
+
+Thread create_thread(Thread_Entry_Point procedure, void *user_pointer, b8 start_as_suspended);
+void join_thread(Thread *thread);
+void detach_thread(Thread *thread);
+void suspend_thread(Thread *thread);
+void resume_thread(Thread *thread);
+void thread_wait_if_suspended(Thread *thread);
+
+
+
+/* ------------------------------------------------ Mutex API ------------------------------------------------ */
+
+#define MUTEX_INTERNAL_STATE_SIZE 40
+
+struct Mutex {
+    u8 platform_data[MUTEX_INTERNAL_STATE_SIZE];
+};
+
+void create_mutex(Mutex *mutex);
+void destroy_mutex(Mutex *mutex);
+void lock(Mutex *mutex);
+void unlock(Mutex *mutex);
