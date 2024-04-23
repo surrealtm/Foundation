96c5508dfde1c5144a082e6f993b54275e2da613
surrealtm <unreal.hiraeth@gmail.com>
2024-04-23 20:20:22 +0200

    Added some triangle / ray intersection tests.

--- src/math/intersect.cpp
+++ src/math/intersect.cpp
@@ -1,5 +1,12 @@
 #include "intersect.h"
 
+
+f32 calculate_triangle_area(const v3<f32> &p0, const v3<f32> &p1, const v3<f32> &p2) {
+    v3<f32> normal = v3_cross_v3(p1 - p0, p2 - p0);
+    return v3_length(normal) / 2;
+}
+
+
 b8 ray_plane_intersection(const v3<f32> &ray_origin, const v3<f32> &ray_direction, const v3<f32> &plane_point, const v3<f32> &plane_normal, f32 *distance) {
     f32 denom = v3_dot_v3(plane_normal, ray_direction);
     if(denom < -0.0f) {
@@ -22,6 +29,8 @@ b8 ray_double_sided_plane_intersection(const v3<f32> &ray_origin, const v3<f32> 
 
     return false;
 }
+
+
 
 f32 point_plane_distance_signed(const v3<f32> &point, const v3<f32> &plane_point, const v3<f32> &plane_normal) {
     return v3_dot_v3(plane_normal, point - plane_point);
@@ -73,4 +82,104 @@ v3<f32> project_point_onto_triangle(const v3<f32> &p, const v3<f32> &a, const v3
     f32 v = v1 * denom;
     f32 w = v2 * denom;
     return a + v * ab + w * ac;
+}
+
+
+
+Triangle_Intersection_Result ray_triangle_intersection(const v3<f32> &ray_origin, const v3<f32> &ray_direction, const v3<f32> &p0, const v3<f32> &p1, const v3<f32> &p2) {
+    //
+    // Copied from:
+    // https://fileadmin.cs.lth.se/cs/Personal/Tomas_Akenine-Moller/code/raytri_tam.pdf
+    //
+    Triangle_Intersection_Result result;
+
+    v3<f32> edge1 = p1 - p0;
+    v3<f32> edge2 = p2 - p0;
+
+    v3<f32> pvector = v3_cross_v3(ray_direction, edge2);
+    f32 determinant = v3_dot_v3(edge1, pvector);
+
+    if(determinant < F32_EPSILON) {
+        // Ray either goes through backfaced triangle or lies on triangle plane.
+        result.intersection = false;
+        return result;
+    }
+
+    v3<f32> tvector = ray_origin - p0;
+    f32 u = v3_dot_v3(tvector, pvector);
+    if(u < 0.0f || u > determinant) {
+        result.intersection = false;
+        return result;
+    }
+
+    v3<f32> qvector = v3_cross_v3(tvector, edge1);
+    f32 v = v3_dot_v3(ray_direction, qvector);
+    if(v < 0.0f || u + v > determinant) {
+        result.intersection = false;
+        return result;
+    }
+
+    f32 inverse_determinant = 1.f / determinant;
+    result.distance = v3_dot_v3(edge2, qvector);
+    result.u        = u * inverse_determinant; // Normalize to [0:1]
+    result.v        = v * inverse_determinant; // Normalize to [0:1]
+    return result;
+}
+
+Triangle_Intersection_Result ray_double_sided_triangle_intersection(const v3<f32> &ray_origin, const v3<f32> &ray_direction, const v3<f32> &p0, const v3<f32> &p1, const v3<f32> &p2) {
+    //
+    // Copied from:
+    // https://fileadmin.cs.lth.se/cs/Personal/Tomas_Akenine-Moller/code/raytri_tam.pdf
+    //
+    Triangle_Intersection_Result result;
+    
+    v3<f32> edge1 = p1 - p0;
+    v3<f32> edge2 = p2 - p0;
+
+    v3<f32> pvector = v3_cross_v3(ray_direction, edge2);
+    f32 determinant = v3_dot_v3(edge1, pvector);
+
+    if(determinant > -F32_EPSILON && determinant < F32_EPSILON) {
+        // Ray lies on triangle plane.
+        result.intersection = false;
+        return result;
+    }
+    
+    v3<f32> tvector = ray_origin - p0;
+    f32 u = v3_dot_v3(tvector, pvector);
+    if(u < 0.0f || u > determinant) {
+        result.intersection = false;
+        return result;
+    }
+
+    v3<f32> qvector = v3_cross_v3(tvector, edge1);
+    f32 v = v3_dot_v3(ray_direction, qvector);
+    if(v < 0.0f || u + v > determinant) {
+        result.intersection = false;
+        return result;
+    }
+
+    f32 inverse_determinant = 1.f / determinant;
+    result.distance = v3_dot_v3(edge2, qvector);
+    result.u        = u * inverse_determinant;
+    result.v        = v * inverse_determinant;
+    return result;
+}
+
+
+void calculate_barycentric_coefficients(const v3<f32> &p0, const v3<f32> &p1, const v3<f32> &p2, const v3<f32> &point, f32 *u, f32 *v, f32 *w) {
+    //
+    // Copied from:
+    // https://users.csc.calpoly.edu/~zwood/teaching/csc471/2017F/barycentric.pdf
+    //
+    v3<f32> n = v3_cross_v3(p1 - p0, p2 - p0);
+    f32 inverse_n2 = 1.f / v3_dot_v3(n, n);
+    
+    v3<f32> t0 = v3_cross_v3(p2 - p1, point - p1);
+    v3<f32> t1 = v3_cross_v3(p0 - p2, point - p2);
+    v3<f32> t2 = v3_cross_v3(p1 - p0, point - p0);
+
+    *u = v3_dot_v3(n, t0) * inverse_n2;
+    *v = v3_dot_v3(n, t1) * inverse_n2;
+    *w = v3_dot_v3(n, t2) * inverse_n2;
 }
--- src/math/intersect.h
+++ src/math/intersect.h
@@ -2,8 +2,21 @@
 #include "foundation.h"
 #include "v3.h"
 
+f32 calculate_triangle_area(const v3<f32> &p0, const v3<f32> &p1, const v3<f32> &p2);
+
 b8 ray_plane_intersection(const v3<f32> &ray_origin, const v3<f32> &ray_direction, const v3<f32> &plane_point, const v3<f32> &plane_normal, f32 *distance);
 b8 ray_double_sided_plane_intersection(const v3<f32> &ray_origin, const v3<f32> &ray_direction, const v3<f32> &plane_point, const v3<f32> &plane_normal, f32 *distance);
 
 f32 point_plane_distance_signed(const v3<f32> &point, const v3<f32> &plane_point, const v3<f32> &plane_normal);
 v3<f32> project_point_onto_triangle(const v3<f32> &p, const v3<f32> &a, const v3<f32> &b, const v3<f32> &c);
+
+struct Triangle_Intersection_Result {
+	b8 intersection; // Whether any intersection actually occured. If not, all other members are undefined.
+	f32 distance; // The unnormalized distance from the ray origin to the intersection point, measured in ray_direction units!
+	f32 u, v; // The normalized barycentric coordinate factors of p1 and p2 respectively, normalized to [0:1].
+};
+
+Triangle_Intersection_Result ray_triangle_intersection(const v3<f32> &ray_origin, const v3<f32> &ray_direction, const v3<f32> &p0, const v3<f32> &p1, const v3<f32> &p2);
+Triangle_Intersection_Result ray_double_sided_triangle_intersection(const v3<f32> &ray_origin, const v3<f32> &ray_direction, const v3<f32> &p0, const v3<f32> &p1, const v3<f32> &p2);
+
+void calculate_barycentric_coefficients(const v3<f32> &p0, const v3<f32> &p1, const v3<f32> &p2, const v3<f32> &point, f32 *u, f32 *v, f32 *w);
