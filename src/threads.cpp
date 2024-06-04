fe1f0810df04cc3a4a6b3d0e9d165f8d0cd03755
surreal <unreal.hiraeth@gmail.com>
2024-06-04 10:44:05 +0200

    Ported over the threads module to linux.

--- src/threads.cpp
+++ src/threads.cpp
@@ -11,21 +11,48 @@ struct Thread_Win32_State {
     CONDITION_VARIABLE signal;
 };
 
-static_assert(sizeof(Thread_Win32_State) <= sizeof(Thread::platform_data), "Thread_Win32_State is bigger than expected.");
-
-void __thread_internal_cleanup(Thread_Win32_State *win32) {
-    if(!win32->setup) return;
-    DeleteCriticalSection(&win32->mutex);
-    win32->handle = INVALID_HANDLE_VALUE;
-    win32->setup = false;
-}
-
-
 struct Mutex_Win32_State {
     CRITICAL_SECTION handle;
 };
 
+static_assert(sizeof(Thread_Win32_State) <= sizeof(Thread::platform_data), "Thread_Win32_State is bigger than expected.");
+
 static_assert(sizeof(Mutex_Win32_State) <= sizeof(Mutex::platform_data), "Mutex_Win32_State is bigger than expected.");
+
+void __thread_internal_cleanup(Thread_Win32_State *state) {
+    if(!state->setup) return;
+    DeleteCriticalSection(&state->mutex);
+    state->handle = INVALID_HANDLE_VALUE;
+    state->setup = false;
+}
+
+
+#elif FOUNDATION_LINUX
+# include <pthread.h>
+# include <unistd.h>
+
+struct Thread_Linux_State {
+    b8 setup;
+    pthread_t id;          // 8  bytes
+    pthread_mutex_t mutex; // 40 bytes
+    pthread_cond_t signal; // 48 bytes
+};
+
+struct Mutex_Linux_State {
+    pthread_mutex_t handle;
+};
+
+static_assert(sizeof(Thread_Linux_State) <= sizeof(Thread::platform_data), "Thread_Linux_State is bigger than expected.");
+
+static_assert(sizeof(Mutex_Linux_State) <= sizeof(Mutex::platform_data), "Mutex_Linux_State is bigger than expected.");
+
+void __thread_internal_cleanup(Thread_Linux_State *state) {
+    if(!state->setup) return;
+    pthread_mutex_destroy(&state->mutex);
+    pthread_cond_destroy(&state->signal);
+    state->id    = -1;
+    state->setup = false;
+}
 
 #endif
 
@@ -37,17 +64,28 @@ Thread create_thread(Thread_Entry_Point procedure, void *user_pointer, b8 start_
     Thread thread;
 
 #if FOUNDATION_WIN32
-    Thread_Win32_State *win32 = (Thread_Win32_State *) thread.platform_data;
-    win32->setup = false;
-    win32->handle = CreateThread(null, 0, procedure, user_pointer, 0, null);
+    Thread_Win32_State *state = (Thread_Win32_State *) thread.platform_data;
+    state->setup = false;
+    state->handle = CreateThread(null, 0, procedure, user_pointer, 0, null);
 
-    if(win32->handle) {
-        InitializeCriticalSection(&win32->mutex);
-        InitializeConditionVariable(&win32->signal);
-        win32->setup = true;
+    if(state->handle) {
+        InitializeCriticalSection(&state->mutex);
+        InitializeConditionVariable(&state->signal);
+        state->setup = true;
         thread.state = THREAD_STATE_Running;
     } else {
-        thread.state = THREAD_STATE_Running; // We should probably give a better error here...
+        thread.state = THREAD_STATE_Terminated; // We should probably give a better error here...
+    }
+#elif FOUNDATION_LINUX
+    Thread_Linux_State *state = (Thread_Linux_State *) thread.platform_data;
+    state->setup = false;
+    if(pthread_create(&state->id, null, (void *(*)(void*)) procedure, user_pointer) == 0 &&
+       pthread_mutex_init(&state->mutex, null) == 0 &&
+       pthread_cond_init(&state->signal, null) == 0) {
+        state->setup = true;
+        thread.state = THREAD_STATE_Running;
+    } else {
+        thread.state = THREAD_STATE_Terminated;
     }
 #endif
 
@@ -69,31 +107,53 @@ void join_thread(Thread *thread) {
     }
 
 #if FOUNDATION_WIN32
-    Thread_Win32_State *win32 = (Thread_Win32_State *) thread->platform_data;
-    if(!win32->setup) return;
+    Thread_Win32_State *state = (Thread_Win32_State *) thread->platform_data;
+    if(!state->setup) return;
 
     b8 this_call_destroyed_thread = false;
     
-    EnterCriticalSection(&win32->mutex);
+    EnterCriticalSection(&state->mutex);
 
     // Make sure the thread has received its resuming signal and is now running again. This prevents
     // the issue where we snatch the signal before the thread_wait_if_suspended procedure, in which case
     // the thread would never awaken, and never properly exit.
     while(thread->state == THREAD_STATE_Resuming) {
-        SleepConditionVariableCS(&win32->signal, &win32->mutex, INFINITE);
+        SleepConditionVariableCS(&state->signal, &state->mutex, INFINITE);
     }
 
     if(thread->state != THREAD_STATE_Terminated) {
         // Only attempt to join if the thread is running, and has not since terminated.
         thread->state = THREAD_STATE_Terminating;
-        WaitForSingleObject(win32->handle, INFINITE);
-        CloseHandle(win32->handle);
+        WaitForSingleObject(state->handle, INFINITE);
+        CloseHandle(state->handle);
         thread->state = THREAD_STATE_Terminated;
         this_call_destroyed_thread = true;
     }
     
-    LeaveCriticalSection(&win32->mutex);
-    if(this_call_destroyed_thread) __thread_internal_cleanup(win32);
+    LeaveCriticalSection(&state->mutex);
+    if(this_call_destroyed_thread) __thread_internal_cleanup(state);
+#elif FOUNDATION_LINUX
+    Thread_Linux_State *state = (Thread_Linux_State *) thread->platform_data;
+    if(!state->setup) return;
+
+    b8 this_call_destroyed_thread = false;
+
+    // Make sure the thread has received its resuming signal and is now running again. This prevents
+    // the issue where we snatch the signal before the thread_wait_if_suspended procedure, in which case
+    // the thread would never awaken, and never properly exit.
+    while(thread->state == THREAD_STATE_Resuming) {
+        pthread_cond_wait(&state->signal, &state->mutex);
+    }
+
+    // Only attempt to join if the thread is running, and has not since terminated.
+    if(thread->state != THREAD_STATE_Terminated) {
+        thread->state = THREAD_STATE_Terminating;
+        pthread_join(state->id, null);
+        thread->state = THREAD_STATE_Terminated;
+    }
+
+    pthread_mutex_unlock(&state->mutex);
+    if(this_call_destroyed_thread) __thread_internal_cleanup(state);
 #endif
 }
 
@@ -101,20 +161,35 @@ void detach_thread(Thread *thread) {
     if(thread->state == THREAD_STATE_Terminated || thread->state == THREAD_STATE_Terminating) return;
 
 #if FOUNDATION_WIN32
-    Thread_Win32_State *win32 = (Thread_Win32_State *) thread->platform_data;
-    if(!win32->setup) return;
+    Thread_Win32_State *state = (Thread_Win32_State *) thread->platform_data;
+    if(!state->setup) return;
 
     b8 this_call_destroyed_thread = true;
     
-    EnterCriticalSection(&win32->mutex);
+    EnterCriticalSection(&state->mutex);
 
     if(thread->state != THREAD_STATE_Terminated && thread->state != THREAD_STATE_Detached) {
-        CloseHandle(win32->handle);
+        CloseHandle(state->handle);
         thread->state = THREAD_STATE_Detached;
     }
 
-    LeaveCriticalSection(&win32->mutex);
-    if(this_call_destroyed_thread) __thread_internal_cleanup(win32);
+    LeaveCriticalSection(&state->mutex);
+    if(this_call_destroyed_thread) __thread_internal_cleanup(state);
+#elif FOUNDATION_LINUX
+    Thread_Linux_State *state = (Thread_Linux_State *) thread->platform_data;
+    if(!state->setup) return;
+
+    b8 this_call_destroyed_thread = true;
+
+    pthread_mutex_lock(&state->mutex);
+
+    if(thread->state != THREAD_STATE_Terminated && thread->state != THREAD_STATE_Detached) {
+        pthread_detach(state->id);
+        thread->state = THREAD_STATE_Detached;
+    }
+
+    pthread_mutex_unlock(&state->mutex);
+    if(this_call_destroyed_thread) __thread_internal_cleanup(state);
 #endif
 }
 
@@ -122,12 +197,19 @@ void suspend_thread(Thread *thread) {
     if(thread->state != THREAD_STATE_Running) return;
 
 #if FOUNDATION_WIN32
-    Thread_Win32_State *win32 = (Thread_Win32_State *) thread->platform_data;
-    if(!win32->setup) return;
+    Thread_Win32_State *state = (Thread_Win32_State *) thread->platform_data;
+    if(!state->setup) return;
 
-    EnterCriticalSection(&win32->mutex);
-    thread->state = THREAD_STATE_Suspending;
-    LeaveCriticalSection(&win32->mutex);
+    EnterCriticalSection(&state->mutex);
+    if(thread->state == THREAD_STATE_Running) thread->state = THREAD_STATE_Suspending;
+    LeaveCriticalSection(&state->mutex);
+#elif FOUNDATION_LINUX
+    Thread_Linux_State *state = (Thread_Linux_State *) thread->platform_data;
+    if(!state->setup) return;
+
+    pthread_mutex_lock(&state->mutex);
+    if(thread->state == THREAD_STATE_Running) thread->state = THREAD_STATE_Suspending;
+    pthread_mutex_unlock(&state->mutex);
 #endif
 }
 
@@ -135,13 +217,21 @@ void resume_thread(Thread *thread) {
     if(thread->state != THREAD_STATE_Suspended) return;
 
 #if FOUNDATION_WIN32
-    Thread_Win32_State *win32 = (Thread_Win32_State *) thread->platform_data;
-    if(!win32->setup) return;
+    Thread_Win32_State *state = (Thread_Win32_State *) thread->platform_data;
+    if(!state->setup) return;
 
-    EnterCriticalSection(&win32->mutex);
-    thread->state = THREAD_STATE_Resuming;
-    WakeAllConditionVariable(&win32->signal);
-    LeaveCriticalSection(&win32->mutex);
+    EnterCriticalSection(&state->mutex);
+    if(thread->state == THREAD_STATE_Suspended) thread->state = THREAD_STATE_Resuming;
+    WakeAllConditionVariable(&state->signal);
+    LeaveCriticalSection(&state->mutex);
+#elif FOUNDATION_LINUX
+    Thread_Linux_State *state = (Thread_Linux_State *) thread->platform_data;
+    if(!state->setup) return;
+
+    pthread_mutex_lock(&state->mutex);
+    if(thread->state == THREAD_STATE_Suspended) thread->state = THREAD_STATE_Resuming;
+    pthread_cond_broadcast(&state->signal);
+    pthread_mutex_unlock(&state->mutex);
 #endif
 }
 
@@ -149,26 +239,42 @@ void thread_wait_if_suspended(Thread *thread) {
     if(thread->state != THREAD_STATE_Suspended && thread->state != THREAD_STATE_Suspending) return;
 
 #if FOUNDATION_WIN32
-    Thread_Win32_State *win32 = (Thread_Win32_State *) thread->platform_data;
-    if(!win32->setup) return;
+    Thread_Win32_State *state = (Thread_Win32_State *) thread->platform_data;
+    if(!state->setup) return;
 
-    EnterCriticalSection(&win32->mutex);
+    EnterCriticalSection(&state->mutex);
     if(thread->state == THREAD_STATE_Suspending) thread->state = THREAD_STATE_Suspended;
 
     while(thread->state == THREAD_STATE_Suspended) {
-        SleepConditionVariableCS(&win32->signal, &win32->mutex, INFINITE);
+        SleepConditionVariableCS(&state->signal, &state->mutex, INFINITE);
     }
 
     thread->state = THREAD_STATE_Running;
-    WakeAllConditionVariable(&win32->signal); // Signal back that we have resumed (e.g. to the join_thread procedure).
+    WakeAllConditionVariable(&state->signal); // Signal back that we have resumed (e.g. to the join_thread procedure).
     
-    LeaveCriticalSection(&win32->mutex);
+    LeaveCriticalSection(&state->mutex);
+#elif FOUNDATION_LINUX
+    Thread_Linux_State *state = (Thread_Linux_State *) thread->platform_data;
+    if(!state->setup) return;
+
+    pthread_mutex_lock(&state->mutex);
+    if(thread->state == THREAD_STATE_Suspending) thread->state = THREAD_STATE_Suspended;
+
+    while(thread->state == THREAD_STATE_Suspended) {
+        pthread_cond_wait(&state->signal, &state->mutex);
+    }
+
+    thread->state = THREAD_STATE_Running;
+    pthread_cond_broadcast(&state->signal);
+    pthread_mutex_unlock(&state->mutex);
 #endif
 }
 
 void thread_sleep(f32 seconds) {
 #if FOUNDATION_WIN32
     Sleep((DWORD) (seconds * 1000.0f));
+#elif FOUNDATION_LINUX
+    usleep((useconds_t) (seconds * 1000000.0f));
 #endif
 }
 
@@ -176,6 +282,8 @@ void thread_sleep(f32 seconds) {
 u32 thread_get_id() {
 #if FOUNDATION_WIN32
     return GetCurrentThreadId();
+#elif FOUNDATION_LINUX
+    return gettid();
 #endif
 }
 
@@ -185,29 +293,41 @@ u32 thread_get_id() {
 
 void create_mutex(Mutex *mutex) {
 #if FOUNDATION_WIN32
-    Mutex_Win32_State *win32 = (Mutex_Win32_State *) mutex->platform_data;
-    InitializeCriticalSection(&win32->handle);
+    Mutex_Win32_State *state = (Mutex_Win32_State *) mutex->platform_data;
+    InitializeCriticalSection(&state->handle);
+#elif FOUNDATION_LINUX
+    Mutex_Linux_State *state = (Mutex_Linux_State *) mutex->platform_data;
+    pthread_mutex_init(&state->handle, null);    
 #endif
 }
 
 void destroy_mutex(Mutex *mutex) {
 #if FOUNDATION_WIN32
-    Mutex_Win32_State *win32 = (Mutex_Win32_State *) mutex->platform_data;
-    DeleteCriticalSection(&win32->handle);
+    Mutex_Win32_State *state = (Mutex_Win32_State *) mutex->platform_data;
+    DeleteCriticalSection(&state->handle);
+#elif FOUNDATION_LINUX
+    Mutex_Linux_State *state = (Mutex_Linux_State *) mutex->platform_data;
+    pthread_mutex_destroy(&state->handle);
 #endif
 }
 
 void lock(Mutex *mutex) {
 #if FOUNDATION_WIN32
-    Mutex_Win32_State *win32 = (Mutex_Win32_State *) mutex->platform_data;
-    EnterCriticalSection(&win32->handle);
+    Mutex_Win32_State *state = (Mutex_Win32_State *) mutex->platform_data;
+    EnterCriticalSection(&state->handle);
+#elif FOUNDATION_LINUX    
+    Mutex_Linux_State *state = (Mutex_Linux_State *) mutex->platform_data;
+    pthread_mutex_lock(&state->handle);
 #endif
 }
 
 void unlock(Mutex *mutex) {
 #if FOUNDATION_WIN32
-    Mutex_Win32_State *win32 = (Mutex_Win32_State *) mutex->platform_data;
-    LeaveCriticalSection(&win32->handle);
+    Mutex_Win32_State *state = (Mutex_Win32_State *) mutex->platform_data;
+    LeaveCriticalSection(&state->handle);
+#elif FOUNDATION_LINUX
+    Mutex_Linux_State *state = (Mutex_Linux_State *) mutex->platform_data;
+    pthread_mutex_unlock(&state->handle);
 #endif
 }
 
@@ -218,5 +338,7 @@ void unlock(Mutex *mutex) {
 u64 interlocked_compare_exchange(u64 volatile *dst, u64 src, u64 cmp) {
 #if FOUNDATION_WIN32
     return InterlockedCompareExchange(dst, src, cmp);
+#elif FOUNDATION_LINUX
+    return __sync_val_compare_and_swap(dst, src, cmp);
 #endif
 }
